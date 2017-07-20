8227f7d63e452a53ea88cc06eea516b86fcc8177
lorenries <lorenries@users.noreply.github.com>
2017-07-20 16:55:30 -0400

    download data from propublica congress api instead of govtrack, add senate members

--- data/download.js
+++ data/download.js
@@ -1,14 +1,33 @@
 
-const axios = require('axios')
-const fs = require('fs')
+const axios = require('axios');
+const fs = require('fs');
 
-axios.get('https://www.govtrack.us/api/v2/role?current=true&limit=540')
+const config = { 
+	headers: {'X-API-Key': 'mUb27VfDvm4bsBphp4YO38jJABcoPeiZ4jCRSu4v'}
+}
+
+axios.get('https://api.propublica.org/congress/v1/115/senate/members.json', config)
   .then(data => {
-    fs.writeFile('./data/people.json', JSON.stringify(data.data.objects), err => {
+  	// console.log(data);
+    fs.writeFile('./data/senate.json', JSON.stringify(data.data.results, null, '\t'), err => {
       if (err) {
         console.log(err)
       } else {
-        console.log(`✅ Saved people (${data.data.objects.length})`)
+      	// console.log(result)
+        console.log(`✅ Saved people (${data.data.results})`)
+      }
+    })
+  })
+
+axios.get('https://api.propublica.org/congress/v1/115/house/members.json', config)
+  .then(data => {
+  	// console.log(data);
+    fs.writeFile('./data/house.json', JSON.stringify(data.data.results, null, '\t'), err => {
+      if (err) {
+        console.log(err)
+      } else {
+      	// console.log(result)
+        console.log(`✅ Saved people (${data.data.results})`)
       }
     })
   })
