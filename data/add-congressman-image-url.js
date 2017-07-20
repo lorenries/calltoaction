610f896cb97379a90f52521b1b77e5fda0a95d42
lorenries <lorenries@users.noreply.github.com>
2017-07-20 16:54:24 -0400

    begin fixing the image download functionality

--- data/add-congressman-image-url.js
+++ data/add-congressman-image-url.js
@@ -2,9 +2,17 @@
 const fs = require('fs')
 const forEach = require('lodash/forEach')
 
-const govData = JSON.parse(
-  fs.readFileSync('./data/people.json', 'utf8')
+const house = JSON.parse(
+  fs.readFileSync('./data/house.json', 'utf8')
 )
+
+const senate = JSON.parse(
+  fs.readFileSync('./data/senate.json', 'utf8')
+)
+
+const houseMembers = house[0].members;
+
+console.log(houseMembers);
 
 const imgRoot = 'https://theunitedstates.io/images/congress'
 const smallImgDimensions = '225x275'
@@ -12,33 +20,74 @@ const largeImgDimensions = '450x550'
 
 let people = []
 
-forEach(govData, (person) => {
-  if (!person.current) {
-    console.error(`${person.person.name} is not a current worker in government.`)
-    return
-  }
+for (var i = 0; i < houseMembers.length; i++) {
+  
+  let bioguideid = houseMembers[i].id;
 
-  // If this isn't a congressman with a district, it is likely a Senator.
-  // We don't want to deal with Senator data just yet.
-  if (!person.district) {
-    console.error(`${person.person.name} is not a Congressman.`)
-    return
-  }
+  // let name = houseMembers[i].first_name + " " + houseMembers[i].last_name;
+  // let district = houseMembers[i].district;
+  // let party = houseMembers[i].party;
+  // let state = houseMembers[i].state;
 
-  const { bioguideid } = person.person
-
-  person.photos = {
+  houseMembers[i].photos = {
     small: `${imgRoot}/${smallImgDimensions}/${bioguideid}.jpg`,
     large: `${imgRoot}/${largeImgDimensions}/${bioguideid}.jpg`
   }
 
-  people.push(person)
-})
+  // let member = {
+  //   "id": bioguideid,
+  //   "name": name,
+  //   "state": state,
+  //   "district": district,
+  //   "party": party,
+  //   "photos": photos
+  // }
 
-fs.writeFile('./data/people.json', JSON.stringify(people), err => {
+  if (houseMembers[i].in_office === true) {
+    people.push(houseMembers[i]);
+  } else {
+    console.log('Not a current member!');
+  }
+
+}
+
+fs.writeFile('./data/people.json', JSON.stringify(people, null, '\t'), err => {
   if (err) {
     console.error(err)
   } else {
     console.log(`✅ Saved people (${people.length})`)
   }
-})
+});
+
+// forEach(houseMembers, (houseMembers) => {
+
+//   // Not needed since Propublica's API returns all current members.
+//   // if (!person.current) {
+//   //   console.error(`${person.person.name} is not a current worker in government.`)
+//   //   return
+//   // }
+
+//   // If this isn't a congressman with a district, it is likely a Senator.
+//   // We don't want to deal with Senator data just yet.
+//   if (!houseMembers.district) {
+//     console.error(`${houseMembers.first_name} is not a Congressman.`)
+//     return
+//   }
+
+//   const { bioguideid } = houseMembers.id;
+
+//   houseMembers.photos = {
+//     small: `${imgRoot}/${smallImgDimensions}/${bioguideid}.jpg`,
+//     large: `${imgRoot}/${largeImgDimensions}/${bioguideid}.jpg`
+//   }
+
+//   houseMembers.push(members)
+// })
+
+// fs.writeFile('./data/people.json', JSON.stringify(people), err => {
+//   if (err) {
+//     console.error(err)
+//   } else {
+//     console.log(`✅ Saved people (${people.length})`)
+//   }
+// })
